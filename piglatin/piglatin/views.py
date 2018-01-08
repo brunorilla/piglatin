8e7dfcd00010b8ccf0cb1d6863aaa6c98720ead9
Bruno <brunorilla9@gmail.com>
2018-01-07 22:05:00 -0300

    second commit

--- piglatin/piglatin/views.py
+++ piglatin/piglatin/views.py
@@ -5,5 +5,18 @@ def home(request):
 	return render(request, 'home.html')
 
 def translate(request):
-	request.GET['origin']
+	original = request.GET['origin'].lower()
+	translation = '	'
+	for word in original().split:
+		if word[0] in ["a","e","i","o","u"]
+			# vowel
+			translation += word
+			translation += 'yay '
+		else:
+			# consonant	
+
+			translation += word[1:]
+			translation += word[0]
+			translation += 'ay '
+
 	return render(request, 'translation.html')	
